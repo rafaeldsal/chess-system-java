fd527a77b3350930a3c79fe31d98a120b72a132e
Rafael Souza <rafaelsnt123@gmail.com>
2022-07-25 21:43:21 -0300

    Printing possible moves

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,11 +2,11 @@ package boardgame;
 
 public class Board {
 
-	private Integer rows;
-	private Integer columns;
+	private int rows;
+	private int columns;
 	private Piece[][] pieces;
 
-	public Board(Integer rows, Integer columns) {
+	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: There must be at least 1 row and 1 column");
 		}
@@ -15,15 +15,15 @@ public class Board {
 		this.pieces = new Piece[rows][columns];
 	}
 
-	public Integer getRows() {
+	public int getRows() {
 		return rows;
 	}
 
-	public Integer getColumns() {
+	public int getColumns() {
 		return columns;
 	}
 
-	public Piece piece(Integer row, Integer column) {
+	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
@@ -59,15 +59,15 @@ public class Board {
 		return aux;
 	}
 
-	private Boolean positionExists(int row, int column) {
+	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 
-	public Boolean positionExists(Position position) {
+	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 
-	public Boolean thereIsApiece(Position position) {
+	public boolean thereIsApiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -11,7 +11,7 @@ public abstract class Piece {
 
 	public Piece(Board board) {
 		this.board = board;
-		this.position = null;
+		position = null;
 	}
 
 	protected Board getBoard() {
