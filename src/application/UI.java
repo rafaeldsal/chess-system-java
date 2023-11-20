35790ee11f01e16f02dead1a866d51cb867f51c6
Rafael Souza <rafaelsnt123@gmail.com>
2023-11-20 19:31:36 -0300

    Queen

--- src/application/UI.java
+++ src/application/UI.java
@@ -101,6 +101,7 @@ public class UI {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 
 			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
 			}
 		} else {
 			System.out.println("CHECKMATE!");
